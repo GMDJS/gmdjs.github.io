4ca15bbdca21d7f08ab66d5347f6236b10c4497b
GriNAME <griname@gmail.com>
2017-02-12 20:48:19 +0300

    Change project structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
 let browserSync   = require('browser-sync'),
 		gulp          = require('gulp'),
-		
+
 		postcss       = require('gulp-postcss'),
 		autoprefixer  = require('autoprefixer'),
 		cssnano       = require('cssnano'),
 		fonts         = require('postcss-font-magician'),
-		
+
 		sass          = require('gulp-sass'),
 		bourbon       = require('node-bourbon'),
 		media         = require('gulp-group-css-media-queries'),
 		notify        = require('gulp-notify'),
 		pug          = require('gulp-pug');
-		
+
 gulp.task('browserSync', function () {
 	browserSync({
 		server: {
-			baseDir: 'assets'
+			baseDir: './'
 		},
 		notify: false
 	})
@@ -27,27 +27,27 @@ gulp.task('postcss', function () {
 			cssnano(),
 			fonts()
 	]);
-	return gulp.src('./assets/sass/*.sass')
+	return gulp.src('./sass/*.sass')
 			.pipe(sass({includePaths: bourbon.includePaths}).on("error", notify.onError()))
 			.pipe(media())
 			.pipe(postcss(processor))
-			.pipe(gulp.dest('./assets/css'))
+			.pipe(gulp.dest('./css'))
 			.pipe(browserSync.reload({stream: true}))
 });
 
 gulp.task('pug', function buildHTML() {
-	return gulp.src('assets/*.pug')
+	return gulp.src('./*.pug')
 			.pipe(pug({
 				pretty: true
 			}).on("error", notify.onError()))
-			.pipe(gulp.dest('assets'))
+			.pipe(gulp.dest('./'))
 			.pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('watch', ['postcss', 'pug', 'browserSync'], function () {
-	gulp.watch('assets/sass/**/*.sass', ['postcss']);
-	gulp.watch(['assets/*.pug', 'assets/pug/**/*.pug'], ['pug']);
-	gulp.watch('assets/js/**/*.js', browserSync.reload);
+	gulp.watch('./sass/**/*.sass', ['postcss']);
+	gulp.watch(['./*.pug', './pug/**/*.pug'], ['pug']);
+	gulp.watch('./js/**/*.js', browserSync.reload);
 });
 
 gulp.task('default', ['watch']);
